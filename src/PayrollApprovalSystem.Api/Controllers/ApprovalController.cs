f358f14ec882101b46b0ca1af61de443cc1d1d1c
Elijah <marcos99.mf@gmail.com>
2026-05-03 20:59:11 +0200

    Add QuestPDF payslip PDF generation

--- src/PayrollApprovalSystem.Api/Controllers/ApprovalController.cs
+++ src/PayrollApprovalSystem.Api/Controllers/ApprovalController.cs
@@ -4,6 +4,7 @@ using PayrollApprovalSystem.Api.DTOs.Approval;
 using PayrollApprovalSystem.Api.Mappings;
 using PayrollApprovalSystem.Application.Services;
 using PayrollApprovalSystem.Domain.Entities;
+using PayrollApprovalSystem.Domain.Interfaces;
 
 namespace PayrollApprovalSystem.Api.Controllers;
 
@@ -12,32 +13,47 @@ namespace PayrollApprovalSystem.Api.Controllers;
 public class ApprovalController : ControllerBase
 {
     private readonly ApprovalService _approvalService;
+    private readonly IPayrollRepository _payrollRepository;
+    private readonly IApprovalRepository _approvalRepository;
 
-    public ApprovalController(ApprovalService approvalService)
+    public ApprovalController(
+        ApprovalService approvalService,
+        IPayrollRepository payrollRepository,
+        IApprovalRepository approvalRepository)
     {
         _approvalService = approvalService;
+        _payrollRepository = payrollRepository;
+        _approvalRepository = approvalRepository;
     }
 
-    [HttpPost("approve")] 
-[Authorize(Roles = "Manager,Admin")]
-[ProducesResponseType(StatusCodes.Status200OK)]
-[ProducesResponseType(StatusCodes.Status400BadRequest)]
-public ActionResult<ApprovalResponseDto> ApprovePayroll([FromBody] ApprovePayrollRequestDto request)    {
-        // TODO: Replace manual Payroll creation with repository/database lookup.
-        var payroll = new Payroll(
-            request.PayrollId,
-            Guid.NewGuid(),
-            4,
-            2026,
-            40000,
-            5000,
-            2000);
+    [HttpPost("approve")]
+    [Authorize(Roles = "Manager,Admin")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<ApprovalResponseDto>> ApprovePayroll([FromBody] ApprovePayrollRequestDto request)
+    {
+        var payroll = await _payrollRepository.GetByIdAsync(request.PayrollId);
+        if (payroll is null)
+        {
+            return NotFound(new
+            {
+                message = $"Payroll with id '{request.PayrollId}' was not found.",
+                type = "NotFound"
+            });
+        }
 
-        // TODO: Replace manual Approval creation with repository/database lookup or persistence. 
-		// TODO: Persist approval status changes through Infrastructure layer.
-        var approval = new Approval(Guid.NewGuid(), payroll.Id);
+        var approval = await _approvalRepository.GetByPayrollIdAsync(payroll.Id);
+        var isNewApproval = approval is null;
+        approval ??= new Approval(Guid.NewGuid(), payroll.Id);
 
         _approvalService.ApprovePayroll(payroll, approval);
+        await _payrollRepository.UpdateAsync(payroll);
+
+        if (isNewApproval)
+            await _approvalRepository.AddAsync(approval);
+        else
+            await _approvalRepository.UpdateAsync(approval);
 
         return Ok(approval.ToDto(payroll));
     }
